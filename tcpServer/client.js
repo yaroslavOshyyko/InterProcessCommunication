ff60ba9b309ff1b548bc9f203c7f74fa739e658f
Yaroslav Oshyyko <yaroslav.oshiyko.5@gmail.com>
2016-05-12 23:48:52 +0300

    Completted all tasks

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -1,3 +1,5 @@
+
+
 var api = {};
 global.api = api;
 api.net = require('net');
@@ -6,13 +8,16 @@ var socket = new api.net.Socket();
 var user;
 
 socket.connect({
-  port: 2000,
+  port: 8080,
   host: '127.0.0.1',
 }, function() {
-  socket.write('Hello from client');
-  socket.on('data', function(data) {
-    user = JSON.parse(data);
-    console.log('Data received (by client): ' + data);
-    console.log('Age of ' + user.name + ' is ' + user.age);
+  socket.on('data', function(data){
+    var task = JSON.parse(data);
+    console.log('Data received (by client)'  + data);
+    var result = task.map(function (item){
+      return item*2;
+    });
+
+    socket.write(JSON.stringify(result));
   });
 });
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,13 +1,31 @@
 var api = {};
 global.api = api;
 api.net = require('net');
+api.cluster = require('cluster');
+api.os = require('os');
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+var results = [];
+var cpuCount = api.os.cpus().length;
+var clientWorkers = [];
+var index = {};
 
 var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
-  console.log('Connected: ' + socket.localAddress);
-  socket.on('data', function(data) {
-    console.log('Data received (by server): ' + data);
+
+
+  clientWorkers.push(socket);
+  index[socket] = clientWorkers.length;
+  var startPoint = (task.length / cpuCount) * (clientWorkers.length - 1);
+  var endPoint = startPoint + (task.length / cpuCount);
+  console.log(startPoint + " " + endPoint);
+  var dataToProcess = JSON.stringify(task.slice(startPoint, endPoint));
+  socket.write(dataToProcess);
+
+  socket.on('data', function(data){
+    console.log('Data received (by server)'  + data);
+    results[index[socket]] = JSON.parse(data);
+    console.log("Result: " + results);
   });
-}).listen(2000);
+
+}).listen(8080);
+
